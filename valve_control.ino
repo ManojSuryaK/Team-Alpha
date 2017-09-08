e74c084624e151ee6b7909e086f54548cab3528b
ManojSuryaK <msd.9052@gmail.com>
2017-09-08 19:59:10 +0530

    Add files via upload

---
+++ valve_control.ino
@@ -0,0 +1,16 @@
+const int A = 8 ;
+const int B = 9 ;
+
+void setup()
+{
+pinMode(A,OUTPUT) ;
+pinMode(B,OUTPUT) ;
+}
+
+void loop()
+{
+delay(3000) ;     
+digitalWrite(A,HIGH) ;
+digitalWrite(B,LOW) ;
+
+ }
